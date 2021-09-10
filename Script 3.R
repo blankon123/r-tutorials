4b3b284e9e795dd054e7248f9eef48c77e3f6560
blankon123 <thosan.girisona@gmail.com>
2021-09-10 18:37:50 +0700

    Add exercises

--- Script 3.R
+++ Script 3.R
@@ -97,3 +97,22 @@ mtcars %>%
 mtcars %>%
   count(cyl) %>% 
   arrange(desc(n))
+
+# 10. Exercise ############################################
+df = import('https://raw.githubusercontent.com/blankon123/r-tutorials/main/top10s.csv')
+View(df)
+
+#1. Find the top 10 energetic song,
+#   show only the song name, artist, year, and the energy
+#   Make sure the song is only show once in the dataframe
+#   sort by the most energetic
+#   Hint : You can use distinct(x,.keep_all=TRUE) to  filter duplicates based on x column
+
+#2. Find the most 'dance-able' song every year,
+#   show only the song name, artist, year, and the dancibility
+#   sort by the year ascending and 'dance-able' descending
+#   Hint : You can use rank(-x, ties.method = "random") to make a ranking 
+
+#3. Find the most popular song every year every genre,
+#   show only the song name, artist, year, and genre
+#   sort by the year descending, if they have the same genre, sort by popularity
